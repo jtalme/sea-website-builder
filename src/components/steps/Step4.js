4415c2825b4c7dff6ffe5b77baa842aa36b42619
jon alme <jon.alme@wheniwork.com>
2018-05-16 23:24:48 -0500

    update title steps to use css to adjust size instead of header tags

--- src/components/steps/Step4.js
+++ src/components/steps/Step4.js
@@ -7,30 +7,25 @@ export default function Step4({title}) {
     <div>
       <h2>{title}</h2>
       <p style={s.p}>
-        Lets open our <code>site.html</code> file with Sublime Text:
-      </p>
-      <ul>
-        <li>navigate to your <code>Desktop/science project site</code> folder</li>
-        <li>right-click the <code>site.html</code> file, and select "open with"</li>
-        <li>from the list of applications, chose "Sublime Text"</li>
-      </ul>
-      <p style={s.p}>
-        Copy and paste the code below into your <code>site.html</code> file.
+        Copy and paste the code below into your <code>site.html</code> file
+        in Caret.
       </p>
       <p style={s.p}>
         <b>Important:</b> Replace the <code>Change me!</code> text with the title
         of your science project.
       </p>
       <Gist id='2ac195082de15a5ff908bfcdb9340730'/>
-      <h3>Viewing your Site</h3>
+      <h3>Viewing Your Changes</h3>
       <p style={s.p}>
-        Lets open our <code>site.html</code> file with Chrome:
+        To view your changes do the following:
       </p>
-      <ul>
-        <li>navigate to your Desktop/science project site folder</li>
-        <li>right-click the <code>site.html</code> file, and select "open with"</li>
-        <li>from the list of applications, chose "Chrome"</li>
-      </ul>
+      <ol>
+        <li>In Caret, save your <code>site.html</code> file</li>
+        <li>
+          In Chrome, refresh the tab where you're viewing the
+          <code>site.html</code> file
+        </li>
+      </ol>
       <p style={s.p}>
         Congratulations! You should now be looking at a white page with
         your science project title at the top! If you look at the browser tab,
@@ -39,8 +34,8 @@ export default function Step4({title}) {
       <h3>Which title is which?</h3>
       <p style={s.p}>
         Change the text in the <code>title</code> tag, and the <code>h1</code> tag
-        to figure out which title corresponds to the browser tab, and the title
-        on the web page.
+        to figure out which title corresponds to the browser tab, and which title
+        is showing on your web page.
       </p>
       <p style={s.p}>
         Move on to the next step to make some additional changes to your title.
--- src/components/steps/Step5.js
+++ src/components/steps/Step5.js
@@ -8,29 +8,30 @@ export default function Step5({title}) {
       <h2>{title}</h2>
       <h3>Size</h3>
       <p style={s.p}>
-        HTML provides many header tags that change the text size. We
-        started with the largest one: <code>h1</code>. Copy and paste
-        the other header tags shown below into your <code>site.html</code>
-        file under the <code>&lt;h1&gt;</code> tag.
+        To change the size of your title, we're going to apply a CSS rule
+        that modifies the <code>&lt;h1&gt;</code> tags on your site:
       </p>
+      <ol>
+        <li>
+          Copy and paste the code below into your <code>style.css</code>
+          file you have open in Caret.
+        </li>
+        <li>Save your <code>style.css</code> file</li>
+        <li>Refresh your <code>site.html</code> tab in Chrome</li>
+      </ol>
       <Gist id="9a560669538062f9787842469380b498"/>
-      <p>
-        Which header tag do you like best? Use that one!
+      <p style={s.p}>
+        Do you notice anything different about your title? Is it huge?!? We
+        applied a rule that made your
+        title <strong>100 pixels large.</strong> Try changing
+        the <code>100px</code> value
+        to <code>1000px</code> or <code>10px</code>. What happens?
       </p>
       <h3>Color</h3>
       <p style={s.p}>
-        Lets open your <code>style.css</code> file in Sublime Text so
-        we can change the color of your title.
-      </p>
-      <ul>
-        <li>navigate to your Desktop/science project site folder</li>
-        <li>right-click the <code>style.css</code> file, and select "open with"</li>
-        <li>from the list of applications, chose "Sublime Text"</li>
-      </ul>
-      <p style={s.p}>
-        Copy and paste the text below into your <code>style.css</code> file
-        and save it. After saving, refresh your site in Chrome, what happened?!?
-        Does that color look familiar?
+        Now lets change the color of your title. Copy and paste the text below into
+        your <code>style.css</code> file you have open in Caret and save it. After saving,
+        refresh your site in Chrome, what happened?!?
       </p>
       <Gist id="fa85441b9cc04870e392850b724c7347"/>
       <p>
@@ -41,6 +42,10 @@ export default function Step5({title}) {
         the page to load your changes.
       </p>
       <a href="https://www.w3schools.com/colors/colors_picker.asp">Color Picker</a>
+      <p style={s.p}>
+        While we're playing with colors, change the color value in
+        the <code>background-color</code> CSS rule too!
+      </p>
       <p style={s.p}>
         Continue on to the next step to change your title font.
       </p>
