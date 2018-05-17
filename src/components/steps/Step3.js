8a273781a4109ad05dc44bc45ca38c08aa9e9b11
jon alme <jon.alme@wheniwork.com>
2018-05-16 22:50:09 -0500

    add steps to create files and open site with chromebook

--- src/components/steps/Step3.js
+++ src/components/steps/Step3.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Glyphicon } from 'react-bootstrap';
 import s from '../../styles/step.style';
 
 export default function Step3({title}) {
@@ -6,24 +7,51 @@ export default function Step3({title}) {
     <div>
       <h3>{title}</h3>
       <p style={s.p}>
-        We'll start by creating a folder that will contain your website
-        HTML and CSS files. On your desktop, create a folder called
-        <code>science project site</code>. Inside the folder we'll need
-        to create two files: <code>site.html</code> and
-        <code>style.css</code>. The folder and files should be structure
-        like this:
+        We'll get started by creating two files in the <code>Downloads</code>
+        folder of your Chromebook by doing the following:
       </p>
-
-      <pre><code>
-      Desktop/<br/>
-      ├── science project site<br/>
-      │   ├── site.html<br/>
-      │   ├── style.css<br/>
-      </code></pre>
-
+      <ol>
+        <li>Open Caret</li>
+        <li>
+          A new file should automatically be created,
+          click <strong>File <Glyphicon glyph="arrow-right" /> Save</strong> and
+          save the file with the filename <code>site.html</code> in the Downloads
+          folder
+        </li>
+        <li>
+          In Caret,
+          click <strong>File <Glyphicon glyph="arrow-right" /> New File</strong> to
+          create another file
+        </li>
+        <li>
+          Now click <strong>File <Glyphicon glyph="arrow-right" /> Save</strong> and
+          save this file with the filename <code>style.css</code> in the Downloads
+          folder
+        </li>
+      </ol>
       <p style={s.p}>
-        Now that we have our files in place, we can begin to write the HTML
-        that will become our site. Continue on to add a title to your website.
+        For the final setup step, lets open our <code>site.html</code> file in
+        Chrome so we can view changes to our page as we make updates.
+      </p>
+      <ol>
+        <li>
+          Click the <Glyphicon glyph="search" /> button on your Chromebook keyboard,
+          type in "File", and open the "Files" app
+        </li>
+        <li>
+          Click on the "Downloads" folder, and find the <code>site.html</code> file
+          you created in the steps above
+        </li>
+        <li>Right-click on the <code>site.html</code> file, and click "View"</li>
+        <li>
+          Congrats, you just opened your site in Chrome! The page is blank for now,
+          like an unpainted canvas, but things are about to get a lot more
+          interesting
+        </li>
+      </ol>
+      <p style={s.p}>
+        Now that we have our files in place, and our <code>site.html</code> file
+        open in Chrome, lets continue on to the next step to add a title
       </p>
     </div>
   );
