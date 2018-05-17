b7e33d9432c0909f5eb7fc0c2842b502b9029aab
jon alme <jon.alme@wheniwork.com>
2018-05-17 01:04:45 -0500

    scroll to top on next step

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
+import ScrollToTop from './components/ScrollToTop';
 
 ReactDOM.render(
   <BrowserRouter basename="/sea-website-builder">
-    <App />
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
   </BrowserRouter>,
   document.getElementById('root'),
 );
